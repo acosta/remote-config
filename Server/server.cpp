d5ac07169346c2e6ced302606c9a7bdafff3eab9
Afonso Costa <arabelo@gmail.com>
2023-12-08 12:43:28 +0100

    The server receives the config data using socket

--- Server/server.cpp
+++ Server/server.cpp
@@ -1,11 +1,15 @@
 #include "server.h"
 
+#include <QDateTime>
 #include <QUdpSocket>
+
+const quint16 SOCKET_PORT = 1454;
 
 Server::Server(QObject *parent)
     : QObject{parent},
     mSocket(new QUdpSocket(this))
 {
+    mSocket->bind(QHostAddress::LocalHost, SOCKET_PORT, QUdpSocket::ShareAddress);
     connect(mSocket, SIGNAL(readyRead()), this, SLOT(handleReceivedConfig()));
 }
 
@@ -19,11 +23,19 @@ Server::~Server()
 
 void Server::handleReceivedConfig()
 {
-    qDebug() << "Received config";
-    QByteArray datagram;
-    while (mSocket->hasPendingDatagrams()) {
-        datagram.resize(mSocket->pendingDatagramSize());
-        mSocket->readDatagram(datagram.data(), datagram.size());
-        qDebug() << "data: " << datagram.constData();
-    }
+    qDebug() << "Received config at " << QDateTime::currentDateTime().toString();
+
+    QByteArray receivedDatagram;
+    do {
+        receivedDatagram.resize(int(mSocket->pendingDatagramSize()));
+        mSocket->readDatagram(receivedDatagram.data(), receivedDatagram.size());
+    } while(mSocket->hasPendingDatagrams());
+
+    quint16 temperature, velocity;
+    QDataStream stream(&receivedDatagram, QIODevice::ReadOnly);
+    stream.setVersion(QDataStream::Qt_6_6);
+    stream >> temperature >> velocity;
+
+    qDebug() << "Temperature: " << temperature << "Velocity: " << velocity;
+
 }
